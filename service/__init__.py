fa8a981c8fbf8f648da930d6c35d3b0605c5cdcd
kenkyking <nadhifbsl@gmail.com>
2026-05-14 12:25:33 -0400

    Fix global Flask app

--- service/__init__.py
+++ service/__init__.py
@@ -1,5 +1,23 @@
 from flask import Flask
+"""
+Package Initialization
+"""
+
+from flask import Flask
 from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+
+@app.after_request
+def set_security_headers(response):
+    """Set security headers"""
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    response.headers["X-Frame-Options"] = "DENY"
+    response.headers["Content-Security-Policy"] = "default-src 'self'"
+    return responsefrom flask_cors import CORS
 
 
 def create_app():
