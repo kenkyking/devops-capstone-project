cc152dfdf927e14b830eb4d8bb40852518d9b957
kenkyking <nadhifbsl@gmail.com>
2026-05-14 12:32:31 -0400

    Fix syntax error in init

--- service/__init__.py
+++ service/__init__.py
@@ -1,4 +1,3 @@
-from flask import Flask
 """
 Package Initialization
 """
@@ -17,21 +16,4 @@ def set_security_headers(response):
     response.headers["X-Content-Type-Options"] = "nosniff"
     response.headers["X-Frame-Options"] = "DENY"
     response.headers["Content-Security-Policy"] = "default-src 'self'"
-    return responsefrom flask_cors import CORS
-
-
-def create_app():
-    """Create and configure the Flask app"""
-    app = Flask(__name__)
-
-    CORS(app)
-
-    @app.after_request
-    def set_security_headers(response):
-        """Set security headers"""
-        response.headers["X-Content-Type-Options"] = "nosniff"
-        response.headers["X-Frame-Options"] = "DENY"
-        response.headers["Content-Security-Policy"] = "default-src 'self'"
-        return response
-
-    return app
+    return response
