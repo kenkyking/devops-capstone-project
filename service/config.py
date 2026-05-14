a6160859c306655008214aab1305f3091b90522a
kenkyking <nadhifbsl@gmail.com>
2026-05-14 12:51:03 -0400

    Fix Flask SQLAlchemy integration

--- service/config.py
+++ service/config.py
@@ -1,18 +1,18 @@
 """
 Global Configuration for Application
 """
+
 import os
 
-# Get configuration from environment
-DATABASE_URI = os.getenv("DATABASE_URI")
+DATABASE_URI = os.getenv(
+    "DATABASE_URI",
+    "sqlite:///test.db"
+)
 
-# Use SQLite for tests if DATABASE_URI is not set
-if not DATABASE_URI:
-    DATABASE_URI = "sqlite:///test.db"
-
-# Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-# Secret for session management
-SECRET_KEY = os.getenv("SECRET_KEY", "s3cr3t-key-shhhh")
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "s3cr3t-key-shhhh"
+)
