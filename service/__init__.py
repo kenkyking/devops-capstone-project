46663f1745357f47bc47a78549266766af11907c
kenkyking <nadhifbsl@gmail.com>
2026-05-14 12:39:12 -0400

    Configure SQLite database

--- service/__init__.py
+++ service/__init__.py
@@ -3,9 +3,15 @@ Package Initialization
 """
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///accounts.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 CORS(app)
 
