74718e4dbe9fe9654e9177df8a7fcf1a975f437f
kenkyking <70520699+kenkyking@users.noreply.github.com>
2026-05-15 14:50:46 +0700

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_talisman import Talisman
 
 
 def create_app():
     """Create and configure the Flask app"""
     app = Flask(__name__)
+
+    talisman = Talisman(app)
 
     CORS(app)
 
